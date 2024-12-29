b2f48e81ab68c31f1241ac54724d3bd434009a6c
Nysxl <briandavidgreyling@gmail.com>
2024-12-30 09:30:42 +1100

    Added tax rates to utilities file from nysxl bounty plugin

--- src/main/java/org/Nysxl/NysxlServerUtils.java
+++ src/main/java/org/Nysxl/NysxlServerUtils.java
@@ -6,6 +6,8 @@ import org.Nysxl.Components.PermissionChecker.DynamicPermissionCheckCommand;
 import org.Nysxl.Components.Profiles.ProfileViewer;
 import org.Nysxl.InventoryManager.DynamicConfigManager;
 import org.Nysxl.InventoryManager.DynamicInventoryHandler;
+import org.Nysxl.Utils.Economy.EconomyManager;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,8 +15,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class NysxlServerUtils extends JavaPlugin {
 
     private CommandRegistry commandRegistry;
-    private static DynamicConfigManager configManager;
+    private static DynamicConfigManager porfileConfigManager;
+    private static DynamicConfigManager economyConfigManager;
     private static Economy economy;
+    private static EconomyManager economyManager;
     private static NysxlServerUtils instance;
 
     @Override
@@ -44,14 +48,64 @@ public class NysxlServerUtils extends JavaPlugin {
     }
 
     private void registerConfigManager() {
-        configManager = new DynamicConfigManager(this);
-        configManager.loadOrCreateDefaultConfig("ProfileViewerConfig"); // Explicitly load the profile config
+        porfileConfigManager = new DynamicConfigManager(this);
+        porfileConfigManager.loadOrCreateDefaultConfig("ProfileViewerConfig"); // Explicitly load the profile config
+
+        economyConfigManager = new DynamicConfigManager(this);
+        economyConfigManager.loadOrCreateDefaultConfig("economy"); // Load the economy config
+
+        economyManager = new EconomyManager(economyConfigManager);
     }
 
+    /*
+        * Register all commands for the plugin
+     */
     private void registerCommands() {
         registerViewRequestsCommand();
         registerReasonCommand();
         registerViewProfileCommand();
+        registerTaxCommand();
+        registerTaxViewerCommand();
+    }
+
+    private void registerTaxCommand() {
+        commandRegistry.createCommand("setTax")
+                .requirePlayer()
+                .check(sender -> sender instanceof Player, "Only players can use this command.")
+                .checkWithArgs((sender, args) -> {
+                    if (args == null || args.length < 1) {
+                        throw new IllegalArgumentException("Usage: /setTax <percentage>");
+                    }
+
+                    double newTax = Double.parseDouble(args[0]);
+                    if (newTax < 0 || newTax > 1) {
+                        throw new IllegalArgumentException("Tax percentage must be between 0 and 1.");
+                    }
+                }, "Invalid arguments.")
+                .onFallback((sender, partial) -> {
+                    sender.sendMessage(ChatColor.RED + "Usage: /setTax <percentage>");
+                })
+                .onExecute((sender, args) -> {
+                    try {
+                        double newTax = Double.parseDouble(args[0]);
+                        economyManager.setTaxRate(newTax);
+                        sender.sendMessage(ChatColor.GREEN + "Tax percentage updated to " + (newTax * 100) + "%.");
+                    } catch (NumberFormatException e) {
+                        sender.sendMessage(ChatColor.RED + "Invalid number format.");
+                    }
+                })
+                .register();
+    }
+
+    private void registerTaxViewerCommand() {
+        commandRegistry.createCommand("getTaxes")
+                .requirePlayer()
+                .withPermission("bounty.gettaxes")
+                .onExecute((sender, args) -> {
+                    double totalTaxes = economyManager.getAvailableTaxes();
+                    sender.sendMessage(ChatColor.GREEN + "Total taxes collected so far: " + totalTaxes);
+                })
+                .register();
     }
 
     private void registerViewRequestsCommand() {
@@ -98,17 +152,28 @@ public class NysxlServerUtils extends JavaPlugin {
                 .register();
     }
 
+
+    //register inventory events
     private void registerInventoryEvents() {
         DynamicInventoryHandler.registerGlobal(this);
 
-        DynamicPermissionCheckCommand permissionHandler = new DynamicPermissionCheckCommand(configManager);
+        DynamicPermissionCheckCommand permissionHandler = new DynamicPermissionCheckCommand(porfileConfigManager);
         permissionHandler.register(this);
     }
 
-    public static DynamicConfigManager getConfigManager() {
-        return configManager;
+
+    //return the profile config manager
+    public static DynamicConfigManager getPorfileConfigManager() {
+        return porfileConfigManager;
     }
 
+    //return the economy config manager
+    public static DynamicConfigManager getEconomyConfigManager() {
+        return economyConfigManager;
+    }
+
+
+    //set up the economy
     private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
@@ -121,11 +186,19 @@ public class NysxlServerUtils extends JavaPlugin {
         return economy != null;
     }
 
+    //returns Vault economy
     public static Economy getEconomy() {
         return economy;
     }
 
+    //returns instance of plugin
     public static NysxlServerUtils getInstance() {
         return instance;
     }
+
+    //returns EconomyManager for easy economy management.
+    public static EconomyManager getEconomyManager() {
+        return economyManager;
+    }
+
 }
---
+++ src/main/java/org/Nysxl/Utils/Economy/EconomyManager.java
@@ -0,0 +1,84 @@
+package org.Nysxl.Utils.Economy;
+
+import org.Nysxl.InventoryManager.DynamicConfigManager;
+import org.Nysxl.NysxlServerUtils;
+import org.bukkit.OfflinePlayer;
+
+public class EconomyManager {
+
+    private double taxRate;
+    private double availableTaxes;
+    private final DynamicConfigManager economyConfigManager;
+
+
+    public EconomyManager(DynamicConfigManager economyConfigManager) {
+        this.economyConfigManager = economyConfigManager;
+        loadTaxRate();
+        loadTotalTaxesPaid();
+    }
+
+    private void saveTotalTaxesPaid() {
+        NysxlServerUtils.getEconomyConfigManager()
+                .getConfig("economy")
+                .set("taxes.availableTaxes", availableTaxes);
+        NysxlServerUtils.getEconomyConfigManager().saveConfig("economy");
+    }
+
+    public void loadTotalTaxesPaid() {
+        availableTaxes = NysxlServerUtils.getEconomyConfigManager()
+                .getConfig("economy")
+                .getDouble("taxes.availableTaxes");
+    }
+
+    private void saveTaxRate() {
+        NysxlServerUtils.getEconomyConfigManager()
+                .getConfig("economy")
+                .set("taxes.taxRate", taxRate);
+        NysxlServerUtils.getEconomyConfigManager().saveConfig("economy");
+    }
+
+    public void loadTaxRate() {
+        taxRate = NysxlServerUtils.getEconomyConfigManager()
+                .getConfig("economy")
+                .getDouble("taxes.taxRate");
+    }
+
+    public double getPlayerBalance(OfflinePlayer player) {
+        return NysxlServerUtils.getEconomy().getBalance(player);
+    }
+
+    public boolean hasBalance(OfflinePlayer player, double balance) {
+        return getPlayerBalance(player) >= balance;
+    }
+
+    public boolean withdrawBalance(OfflinePlayer player, double balance) {
+        return NysxlServerUtils.getEconomy().withdrawPlayer(player, balance).transactionSuccess();
+    }
+
+    public boolean depositBalance(OfflinePlayer player, double balance) {
+        return NysxlServerUtils.getEconomy().depositPlayer(player, balance).transactionSuccess();
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public double getAvailableTaxes() {
+        return availableTaxes;
+    }
+
+    public void setTaxRate(double taxRate) {
+        this.taxRate = taxRate;
+        saveTaxRate();
+    }
+
+    public void setAvailableTaxes(double availableTaxes) {
+        this.availableTaxes = availableTaxes;
+        saveTotalTaxesPaid();
+    }
+
+    public void addToAvailableTaxes(double amount) {
+        availableTaxes += amount;
+        saveTotalTaxesPaid();
+    }
+}
